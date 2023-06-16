92c646e345cdad1cfae452fd354137de39e9721a
LES8638 <rlacodlf11@naver.com>
2023-06-16 13:51:37 +0900

    로그인페이지,메인페이지 프로토타입

--- app/src/main/java/com/example/honcook/MainActivity.kt
+++ app/src/main/java/com/example/honcook/MainActivity.kt
@@ -3,22 +3,24 @@ package com.example.honcook
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.honcook.databinding.ActivityMainBinding
+import com.example.honcook.databinding.MainpageBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var loginbinding: ActivityMainBinding
+    private lateinit var mainbinding: MainpageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        loginbinding = ActivityMainBinding.inflate(layoutInflater)
+        mainbinding = MainpageBinding.inflate(layoutInflater)
+        val loginview = loginbinding.root
+        val mainview = mainbinding.root
 //        setContentView(R.layout.activity_main)
-        setContentView(view)
-        var count=0
-        binding.button1.setOnClickListener{
-            count++
-            binding.text01.text = count.toString()
-            binding.button1.text = "inc count"
-
+        setContentView(loginview)
+        loginbinding.loginbutton.setOnClickListener{
+            setContentView(mainview)
+        }
+        mainbinding.logoutbutton.setOnClickListener{
+            setContentView(loginview)
         }
     }
 }
